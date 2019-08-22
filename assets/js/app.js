6ba8c53c7c73fb9088d23f023382c63d0f849c75
Nestor Bonilla <nbonilla@localhost.localdomain>
2019-08-21 18:27:31 -0600

    add click action

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,18 @@
-function cargarDatos(){
-    var datos = Mustache.render(data[0].slide_1.tpl);
-    $('#section').html(datos);
+function cargarDatos(slide){
+    var datos = Mustache.render(data[0][slide].tpl);
+    $('#webslides').html(datos);
 }
 
+
+
 $( document ).ready(function() {
-    cargarDatos();
+    cargarDatos("slide_1");
 
     $('#path55').on("click", function () {
         alert('hello');
+    });
+    
+    $('#path55-5').on("click", function () {
+       cargarDatos("slide_2");
     });
 });
