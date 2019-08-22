70ec36e1700358f7a4161eb242dc1289844184a7
Porfirio Páiz C <porfiriopaiz@gmail.com>
2019-08-22 15:44:41 -0600

    Fix another merge conflict

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,21 @@
-function cargarDatos(slide){
-    var datos = Mustache.render(data[0][slide].tpl);
+function cargarDatos(slices){
+    var datos = Mustache.render(data[0][slices].tpl);
     $('#webslides').html(datos);
 }
 
 $( document ).ready(function() {
-    cargarDatos("slide_1");
+    // cargarDatos("slide_1");
 
     $('#path55').on("click", function () {
         alert('hello');
     });
-<<<<<<< HEAD
-    
-    
-=======
 
->>>>>>> 02719f197597450a6af6e8b79ff1fedae45d49af
     $('#path55-5').on("click", function () {
-       cargarDatos("slide_2");
+    // cargarDatos("slide_2");
     });
-    
+
 //    Derecha-Alumnos
     $("#path40").on('click', function(){
-       alert("flecha"); 
+       alert("flecha");
     });
 });
